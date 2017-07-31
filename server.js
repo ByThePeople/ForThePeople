beaefae36a2fef25ae52cbd69ddd5ef026abe6f7
Drew Biehle <drew.biehle@gmail.com>
2017-07-31 16:27:27 -0700

    Added express, updated server.js to require express and listen

--- server.js
+++ server.js
@@ -4,3 +4,12 @@ const express = require('express');
 const app = express();
 
 app.use(express.static('./public'));
+
+const PORT = process.env.PORT || 3000;
+
+// Add connection to Postgres here
+// const connectionString = process.env.CONNECTION_STRING || `postgres://postgres:${process.env.PG_PASSWORD}@localhost:5432/reps`;
+
+app.listen(PORT, function() {
+  console.log(`Listening on port ${PORT}`)
+})
