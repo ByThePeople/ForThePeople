2a132d1056c40327d25ace843f41a99cfef57d78
Drew Biehle <drew.biehle@gmail.com>
2017-08-01 23:00:26 -0700

    Added print function tied to a print button for the letter form

--- public/scripts/controller/contactInfoController.js
+++ public/scripts/controller/contactInfoController.js
@@ -1,3 +1,30 @@
 /* jshint browser: true, devel: true, esversion: 6 */
 
 'use strict';
+
+var app = app || {};
+
+(function(module){
+
+  const formAsLetter = {};
+
+  function printSection(divName) {
+    var printContents = $(divName).html();
+    var w = window.open();
+    w.document.write(printContents);
+    w.print();
+    w.close();
+  }
+
+  formAsLetter.submit = () => {
+    $('#print').on('click', (event) => {
+  console.log('just after click registered')
+      event.preventDefault();
+      printSection('#email-input');
+    })
+  }
+  formAsLetter.submit();
+
+  module.formAsLetter = formAsLetter;
+
+})(app)
