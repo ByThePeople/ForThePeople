55a4609cb41a71bc7e401c30fc83f02680c023a3
ren burnett <elcreature@live.com>
2017-08-03 09:11:51 -0700

    got issues and date sql logic to console.log in terminal, almost there yall!

--- server.js
+++ server.js
@@ -6,10 +6,11 @@ const express = require('express');
 const pg = require('pg');
 const app = express();
 const fs = require('fs');
-
+const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
 
-const connectionString = process.env.DATABASE_URL || `postgres://postgres:${process.env.PG_PASSWORD}@localhost:5432/forthepeople`;
+const connectionString = process.env.DATABASE_URL || `postgres://localhost:5432/forthepeople`;
+// const connectionString = process.env.DATABASE_URL || `postgres://postgres:${process.env.PG_PASSWORD}@localhost:5432/forthepeople`;
 
 const client = new pg.Client(connectionString);
 client.connect();
@@ -17,66 +18,56 @@ client.connect();
 client.on('error', err => console.error(err));
 
 app.use(express.static('./public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 const PORT = process.env.PORT || 3000;
 
-//
-// app.post('/issues', function(req, res){
-// console.log(req);
-//   client.query(`
-//     INSERT INTO issues
-//     (issues, userdate) VALUES($1, $2) ON CONFLICT DO NOTHING;`,
-//     [
-//       req.body.issues,
-//       req.body.userdate
-//     ])
-//     .catch(console.error);
-// })
 
 app.post('/issues', function(req, res){
-console.log(req);
+console.log(req.body);
   client.query(`
     INSERT INTO issues
-    (issues) VALUES($1) ON CONFLICT DO NOTHING;`,
+    (issues, userdate) VALUES($1, $2) ON CONFLICT DO NOTHING;`,
     [
-      req.body.issues/*,
-      req.body.userdate*/
+      req.body.issues,
+      req.body.userdate
     ])
     .catch(console.error);
 })
 
-/* nodemailer api language
-***************************************************/
-// create reusable transporter object using the default SMTP transport
-let transporter = nodemailer.createTransport({
-  host: 'smtp-mail.outlook.com',
-  secureConnection: false, // TLS requires secureConnection to be false
-  port: 587, // port for secure SMTP
-  tls: {ciphers:'SSLv3'},
-  auth: {
-    user: 'bythepeople_forthepeople@outlook.com',
-    pass: `${process.env.OUTLOOK_PASSWORD}`
-  }
-});
-
-// setup email data
-let mailOptions = {
-  from: 'bythepeople_forthepeople@outlook.com', // sender address
-  to: '', // list of receivers
-  subject: 'Hello ✔', // Subject line
-  text: 'Hello world?' // plain text body could instead use 'html: ' with html tags
-};
-
-// send mail with defined transport object
-transporter.sendMail(mailOptions, (error, info) => {
-  if (error) {
-    return console.log(error);
-  }
-  console.log('Message %s sent: %s', info.messageId, info.response);
-});
-
-/* end nodemailer section
-******************************************************/
+// /* nodemailer api language
+// ***************************************************/
+// // create reusable transporter object using the default SMTP transport
+// let transporter = nodemailer.createTransport({
+//   host: 'smtp-mail.outlook.com',
+//   secureConnection: false, // TLS requires secureConnection to be false
+//   port: 587, // port for secure SMTP
+//   tls: {ciphers:'SSLv3'},
+//   auth: {
+//     user: 'bythepeople_forthepeople@outlook.com',
+//     pass: `${process.env.OUTLOOK_PASSWORD}`
+//   }
+// });
+//
+// // setup email data
+// let mailOptions = {
+//   from: 'bythepeople_forthepeople@outlook.com', // sender address
+//   to: '', // list of receivers
+//   subject: 'Hello ✔', // Subject line
+//   text: 'Hello world?' // plain text body could instead use 'html: ' with html tags
+// };
+//
+// // send mail with defined transport object
+// transporter.sendMail(mailOptions, (error, info) => {
+//   if (error) {
+//     return console.log(error);
+//   }
+//   console.log('Message %s sent: %s', info.messageId, info.response);
+// });
+//
+// /* end nodemailer section
+// ******************************************************/
 
 //DATABASE LOADERS
 //Adapted from codefellows301 lab 13
