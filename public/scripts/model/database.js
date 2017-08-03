d24d1bb7fe1636cabdd5d4af84853922efba9028
britinimalloy <britini.malloy@outlook.com>
2017-08-02 18:08:09 -0700

    added routing functionality but it doesnt twerk yall

---
+++ public/scripts/model/database.js
@@ -0,0 +1,21 @@
+/* jshint browser: true, devel: true, esversion: 6 */
+
+'use strict';
+
+var app = app || {};
+
+(function(module){
+
+  const userIssues = {};
+
+//dont forget when this is called pass it an obj
+  userIssues.insertIssues = function(userObject) {
+    console.log(userObject);
+    $.post('/issues', {
+      issue: userObject.issues/*,
+      userdate: userObject.userdate*/
+    }).then(console.log('inside insertIssues'))
+  }
+
+  module.userIssues = userIssues;
+})(app)
